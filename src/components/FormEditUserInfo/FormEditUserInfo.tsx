d4dc4657eebb1ab6c1d4a95847d0357bd34c244b
unknown <lucasjulioalves@gmail.com>
2024-02-23 21:07:10 -0300

    adicionado form

---
+++ src/components/FormEditUserInfo/FormEditUserInfo.tsx
@@ -0,0 +1,62 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import "./FormEditUserInfo.css";
+import User from "../../model/User";
+
+interface UserFormInput {
+    name: string;
+    phoneNumber: string;
+    gender: string;
+    email: string;
+    document: string;
+    birth: string;
+}
+
+type OnSubmitFunction = (user: User) => void;
+
+function FormEditUserInfo( props : {
+    onSubmit : OnSubmitFunction,
+    user: User
+}){
+    const onSubmitHandler: SubmitHandler<UserFormInput> = (data) => update(data)
+    const { register, handleSubmit} = useForm<UserFormInput>({
+        defaultValues: {
+            name: props.user.name,
+            phoneNumber: props.user.phoneNumber,
+            gender: props.user.gender,
+            email: props.user.email,
+            document: props.user.document,
+            birth: props.user.birth
+        }
+    });
+
+    const update = (data: UserFormInput) => {
+        
+        props.onSubmit({
+            name: data.name,
+            phoneNumber: data.phoneNumber,
+            gender: data.gender,
+            email: data.email,
+            document: data.document,
+            birth: data.birth
+        } as User)
+    }
+    return(<>
+    <form className="form-update-user-info" onSubmit={handleSubmit(onSubmitHandler)}>
+
+        <input  {...register("name", { required: "Name cannot be empty"})} placeholder="Name"/>
+        <input  {...register("phoneNumber", { required: "Phone number cannot be empty"})} placeholder="Phone Number" />
+        <input  {...register("gender", { required: "gender cannot be empty"})} placeholder="Gender"/>
+        <input {...register("email", {
+            pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address"
+            }
+        })} placeholder="E-Mail"/>
+        <input  {...register("document", { required: "Document cannot be empty"})} placeholder="Document" />
+        <input  {...register("birth", { required: "Birth cannot be empty"})}  placeholder="Birth"/>
+        <input type="submit" />
+      </form>
+    </>);
+}
+
+export default FormEditUserInfo;
--- src/pages/UserInfoPage/UserInfoPage.tsx
+++ src/pages/UserInfoPage/UserInfoPage.tsx
@@ -5,10 +5,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import "./UserInfoPage.css";
 import { useNavigate } from "react-router-dom";
+import FormEditUserInfo from "../../components/FormEditUserInfo/FormEditUserInfo";
 
 function UserInfoPage() {
 
     const [user, setUser] = useState({} as User);
+    const [isEditing, setIsEditing] = useState(false);
     const navigate = useNavigate();
     const loggedUser : User = useSelector((state: RootState) => state.user.value);
 
@@ -23,14 +25,24 @@ function UserInfoPage() {
         })
     },[])
 
+    const updateUser = (user: User) => {
+        console.log(user);
+        setIsEditing(false);
+    }
+    const info = () => {
+        return(<>
+            <p>Name: {user.name}</p>
+            <p>Phone Number: {user.phoneNumber}</p>
+            <p>Gender: {user.gender}</p>
+            <p>Email: {user.email}</p>
+            <p>Document: {user.document}</p>
+            <p>Birth: {user.birth}</p>
+            <button onClick={() => setIsEditing(true)} >Edit</button>
+        </>)
+    }
     
     return(<>
-        <p>Name: {user.name}</p>
-        <p>Phone Number: {user.phoneNumber}</p>
-        <p>Gender: {user.gender}</p>
-        <p>Email: {user.email}</p>
-        <p>Document: {user.document}</p>
-        <p>Birth: {user.birth}</p>
+        {isEditing ? <FormEditUserInfo onSubmit={updateUser} user={user} ></FormEditUserInfo> : info()}
     </>)
 }
 
